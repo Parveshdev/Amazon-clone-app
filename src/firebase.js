073bd3a7564854d93d9c07e52c13bebd8f05fe7d
parvesh k <parveshk@parveshs-MacBook-Air.local>
2022-10-25 23:16:12 +0530

    importing router

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBg3TliKpIurSKnkdntD6n2trRSfeU7g7I",
+  authDomain: "clone-1c2a7.firebaseapp.com",
+  projectId: "clone-1c2a7",
+  storageBucket: "clone-1c2a7.appspot.com",
+  messagingSenderId: "78397121315",
+  appId: "1:78397121315:web:199f20f2292275fd1de296",
+  measurementId: "G-M9CG46FE31",
+};
